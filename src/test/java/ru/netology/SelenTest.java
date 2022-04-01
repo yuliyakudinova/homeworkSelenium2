0e0ddbeb5b6a3ae69b7dd545e032274e841c0532
Yuliya Kudinova <yulechka.kudinova.1991@mail.ru>
2022-04-01 21:38:21 +0300

    chan

--- src/test/java/ru/netology/SelenTest.java
+++ src/test/java/ru/netology/SelenTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +17,11 @@ public class SelenTest {
 
       @BeforeAll
      public static void setUpAll(){
-         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+          WebDriverManager.chromedriver().setup();
         }
 
      @BeforeEach
     public void setUp() {
-
           driver = new ChromeDriver();
      }
 
